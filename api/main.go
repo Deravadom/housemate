e2528e35945224d2d08477dd0e53007ed834f9e0
Michael Needleman <michael.needleman01@gmail.com>
2024-05-27 17:57:35 -0700

    Set up Go with Gin framework and test basic syntax

---
+++ api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []Album{
+	{ID: "1", Title: "Blu Train", Artist: "Person", Price: 100.05},
+}
+
+func getAlbums(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.Run("localhost:3000")
+}
