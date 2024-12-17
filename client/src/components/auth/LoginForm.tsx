ec6b2e89f530d035a70e26815ae40291813a3275
Michael Needleman <michael.needleman01@gmail.com>
2024-12-17 13:22:19 -0800

    Make login form somewhat nicer

--- client/src/components/auth/LoginForm.tsx
+++ client/src/components/auth/LoginForm.tsx
@@ -20,26 +20,28 @@ const LoginForm = ({ to }: Props) => {
   }
 
   return (
-    <>
+    <div className="flex flex-column w-100 items-center">
       <div className="f1 flex flex-row justify-center bg-moon-gray w-100 h3 mb3">
         <span className="f3 self-center">Welcome to Housemate</span>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-column w5">
-        <label>Email</label>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-column ba br3 ph2 pv2 b--white">
+        <label className="mr-auto">Email</label>
         <input
           {...register("email", { required: true })}
           aria-invalid={errors.email ? "true" : "false"}
+          className="mb3"
         />
         {errors.email?.type === 'required' && <p role="alert">Email is required</p>}
         <label>Password</label>
         <input
           {...register("password", { required: true })}
           aria-invalid={errors.password ? "true" : "false"}
+          className="mb3"
         />
         {errors.password?.type === 'required' && <p role="alert">Password is required</p>}
-        <input type="submit" value="Log In" />
+        <input type="submit" value="Log In" className="w3 ml-auto"/>
       </form>
-    </>
+    </div>
   )
 }
 
