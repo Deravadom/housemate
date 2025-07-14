6ea55338c222119a0ac04cc19e8585af916131e9
Michael Needleman <michael.needleman01@gmail.com>
2025-07-14 11:52:47 -0700

    Move apollo provider to _layout and remove app

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,31 @@
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Stack } from 'expo-router';
+import * as SecureStore from "expo-secure-store";
+
+const httpLink = new HttpLink({
+  uri: 'https://light-promptly-primate.ngrok-free.app/graphql'
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await SecureStore.getItemAsync('housemate-bearer');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <ApolloProvider client={client}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </ApolloProvider>
+  )
 }
