bc423e77b81cba64617c4cad75ab9a84b067c5fc
Michael Needleman <michael.needleman01@gmail.com>
2025-07-15 16:41:35 -0700

    Add extra household fields

--- api/app/graphql/mutations/household_mutation_type.rb
+++ api/app/graphql/mutations/household_mutation_type.rb
@@ -10,22 +10,70 @@ module Mutations
       field :set_household, Types::HouseholdType do
         argument :id, GraphQL::Types::ID, required: true
       end
+
+      field :create_household, Types::HouseholdType do
+        argument :name, String, required: true
+      end
+
+      field :delete_household, Boolean do
+        argument :id, GraphQL::Types::ID, required: true
+      end
+
+      field :add_user_to_household, Boolean do
+        argument :id, GraphQL::Types::ID, required: true
+        argument :user_id, GraphQL::Types::ID, required: true
+      end
+
+      field :remove_user_from_household, Boolean do
+        argument :id, GraphQL::Types::ID, required: true
+        argument :user_id, GraphQL::Types::ID, required: true
+      end
     end
 
     def edit_household(name:)
-      return unless (user = context[:current_user])
-      return unless (household = user.household)
+      return unless current_user
+      return unless (household = current_user.household)
 
       household.update(name: name)
     end
 
     def set_household(id:)
-      return unless (user = context[:current_user])
-      return unless (household = user.households.find(id))
+      return unless current_user
+      return unless (household = current_user.households.find(id))
 
-      user.update(household: household)
+      current_user.update(household: household)
 
       household
+    end
+
+    def create_household(name:)
+      return unless current_user
+      household = Household.create(name: name)
+      current_user.households << household
+      household
+    end
+
+    def delete_household(id:)
+      return unless current_user
+      return unless (household = current_user.households.find(id))
+
+      household.destroy
+    end
+
+    def add_user_to_household(id:, user_id:)
+      return unless current_user
+      return unless (household = current_user.households.find(id))
+      return unless (user = User.find(user_id))
+
+      household.users << user
+    end
+
+    def remove_user_from_household(id:, user_id:)
+      return unless current_user
+      return unless (household = current_user.households.find(id))
+      return unless (user = User.find(user_id))
+
+      household.users.delete(user)
     end
   end
 end
