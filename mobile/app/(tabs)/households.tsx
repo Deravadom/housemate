caf1bc7ca1f92308b2f177249d6b9b8cbc501287
Michael Needleman <michael.needleman01@gmail.com>
2025-07-15 16:41:23 -0700

    Add simple household listing

--- mobile/app/(tabs)/households.tsx
+++ mobile/app/(tabs)/households.tsx
@@ -1,12 +1,25 @@
-import { Text, View } from 'react-native';
-// TODO: Import and use the relevant Apollo query for households
+import { useHouseholdsQuery } from '@/__generated__/types';
+import { Text, View, TouchableOpacity } from 'react-native';
 
 const HouseholdsPage = () => {
-  // TODO: Use Apollo query to fetch households
+  const { data } = useHouseholdsQuery()
+  console.log(data)
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">Households</Text>
-      {/* TODO: Render households here */}
+      {data?.households?.map((household) => {
+        return (
+          <TouchableOpacity
+            className="p-4 border border-gray-300 rounded-md"
+            key={household.id}
+            onPress={() => {
+              console.log(household)
+            }}
+          >
+            <Text>{household.name}</Text>
+          </TouchableOpacity>
+        )
+      })}
     </View>
   );
 };
